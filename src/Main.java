16c3857095fcc5fecae824818b313481d06c88cc
Sergey <moskovets2319@yandex.ru>
2023-05-12 01:37:56 +0300

    использовать утилитарный класс

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,57 @@
 import model.Epic;
 import model.Subtask;
 import model.Task;
-import service.Manager;
+import utils.Managers;
 
 public class Main {
 
     public static void main(String[] args) {
-        Manager manager = new Manager();
+
+        Managers managers = new Managers();
+
         Task task1 = new Task("отдохнуть", "набраться сил перед программированием");
-        manager.createNewTask(task1);
+        managers.getDefault().createNewTask(task1);
 
         Task task2 = new Task("покормить кота", "насыпать корм в миску");
-        manager.createNewTask(task2);
+        managers.getDefault().createNewTask(task2);
 
         Epic epic1 = new Epic("сделать ТЗ № 3", "реализовать менеджер задач");
-        manager.createNewEpic(epic1);
+        managers.getDefault().createNewEpic(epic1);
 
         Subtask epic1Subtask1 = new Subtask(3, "написать код", "реализовать логику работы программы");
-        manager.createNewSubtask(epic1, epic1Subtask1);
+        managers.getDefault().createNewSubtask(epic1, epic1Subtask1);
 
         Subtask epic1Subtask2 = new Subtask(3, "проверить ошибки", "добавить задачи и исправить оишбки");
-        manager.createNewSubtask(epic1, epic1Subtask2);
+        managers.getDefault().createNewSubtask(epic1, epic1Subtask2);
 
         Epic epic2 = new Epic("отправить на проверку", "если работа готова");
-        manager.createNewEpic(epic2);
+        managers.getDefault().createNewEpic(epic2);
 
         Subtask epic2Subtask1 = new Subtask(6, "загрузить на GitHub", "загрузить через bash");
-        manager.createNewSubtask(epic2, epic2Subtask1);
+        managers.getDefault().createNewSubtask(epic2, epic2Subtask1);
 
-        manager.getList();
-        manager.getAllTasks();
-        manager.getAllEpics();
-        manager.getAllSubtasks();
-        manager.getTaskById(2);
-        manager.getEpicById(6);
-        manager.getSubtaskById(4);
-        manager.updateTask(task1);
-        manager.updateEpic(epic2);
-        manager.updateSubtask(epic1Subtask2);
-        manager.getSubtasksOfEpic(epic1);
-        manager.deleteById(4);
-        manager.deleteAllTasks();
-        manager.deleteAllEpics();
-        manager.deleteAllSubtasks();
-        manager.deleteAll();
+        managers.getDefault().getList();
+        managers.getDefaultHistory().add(managers.getDefault().getTask(1));
+        managers.getDefaultHistory().add(managers.getDefault().getTask(2));
+        managers.getDefaultHistory().add(managers.getDefault().getEpic(3));
+        managers.getDefaultHistory().getHistory();
+        managers.getDefaultHistory().add(managers.getDefault().getSubtask(4));
+        managers.getDefaultHistory().add(managers.getDefault().getSubtask(5));
+        managers.getDefaultHistory().add(managers.getDefault().getEpic(6));
+        managers.getDefaultHistory().add(managers.getDefault().getSubtask(7));
+        managers.getDefaultHistory().add(managers.getDefault().getTask(1));
+        managers.getDefaultHistory().add(managers.getDefault().getTask(2));
+        managers.getDefaultHistory().add(managers.getDefault().getEpic(3));
+        managers.getDefaultHistory().add(managers.getDefault().getSubtask(4));
+        managers.getDefaultHistory().getHistory();
+        managers.getDefault().updateTask(task1);
+        managers.getDefault().updateEpic(epic2);
+        managers.getDefault().updateSubtask(epic1Subtask2);
+        managers.getDefault().getSubtasksOfEpic(epic1);
+        managers.getDefault().deleteById(4);
+        managers.getDefault().deleteAllTasks();
+        managers.getDefault().deleteAllEpics();
+        managers.getDefault().deleteAllSubtasks();
+        managers.getDefault().deleteAll();
     }
 }
