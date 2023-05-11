a3be579ea9a2d4f1804de1913cb449a78743b226
Sergey <moskovets2319@yandex.ru>
2023-05-12 01:27:09 +0300

    создать интерфейс TaskManager

---
+++ src/service/TaskManager.java
@@ -0,0 +1,53 @@
+package service;
+
+import model.Epic;
+import model.Subtask;
+import model.Task;
+
+import java.util.List;
+
+public interface TaskManager {
+
+    void createNewTask(Task task);
+
+    void createNewEpic(Epic epic);
+
+    void createNewSubtask(Epic epic, Subtask subtask);
+
+    void getList();
+
+    List<Task> getAllTasks();
+
+    List<Epic> getAllEpics();
+
+    List<Subtask> getAllSubtasks();
+
+    void deleteAll();
+
+    void deleteAllTasks();
+
+    void deleteAllEpics();
+
+    void deleteAllSubtasks();
+
+    Task getTask(int number);
+
+    Epic getEpic(int number);
+
+    Subtask getSubtask(int number);
+
+    void updateTask(Task task);
+
+    void updateEpic(Epic epic);
+
+    void updateSubtask(Subtask subtask);
+
+    void updateEpicStatus(Epic epic);
+
+    void deleteById(int idInput);
+
+    void getSubtasksOfEpic(Epic epic);
+
+    Epic getEpicOfSubtask(Subtask subtask);
+
+}
