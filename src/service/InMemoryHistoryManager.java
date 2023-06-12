a4063bdb8430f4231ac9f1b8eb0ca9cae4567401
Sergey <moskovets2319@yandex.ru>
2023-06-12 22:25:18 +0300

    реализовать CustomLinkedList и его методы

--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -2,28 +2,89 @@ package service;
 
 import model.Task;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    private final List<Task> taskHistory = new LinkedList<>();
+    HashMap<Integer, Node<Task>> taskHistoryNodes = new HashMap<>();
 
-    private static final int MAX_HISTORY_SIZE = 10;
+    Node<Task> head;
+    Node<Task> tail;
+    int size = 0;
 
+    void linkLast(Task task) {
+        Node<Task> oldTail = tail;
+        Node<Task> newNode = new Node<>(oldTail, task, null);
+        tail = newNode;
+        if (oldTail == null) {
+            head = newNode;
+        }
+        else {
+            oldTail.next = newNode;
+        }
+        size++;
+        Task newTask = newNode.task;
+        taskHistoryNodes.put(newTask.id, newNode);
+    }
+
+    List<Task> getTasks() {
+        ArrayList<Task> taskHistory= new ArrayList<>();
+        Node<Task> taskNode = head;
+        while (taskNode != null) {
+            taskHistory.add(taskNode.task);
+            taskNode = taskNode.next;
+        }
+        return taskHistory;
+    }
+
+    void removeNode(Node<Task> taskNode) {
+        Node<Task> prevNode = taskNode.prev;
+        Node<Task> nextNode = taskNode.next;
+
+        if (prevNode == null) {
+            head = nextNode;
+        } else {
+            prevNode.next = nextNode;
+            taskNode.prev = null;
+        }
+
+        if (nextNode == null) {
+            tail = prevNode;
+        } else {
+            nextNode.prev = prevNode;
+            taskNode.next = null;
+        }
+
+        taskNode.task = null;
+        size--;
+    }
 
     @Override
     public void add(Task task) {
-        if (taskHistory.size() < MAX_HISTORY_SIZE) {
-            taskHistory.add(task);
-        } else {
-            taskHistory.remove(0);
-            taskHistory.add(task);
+        if (taskHistoryNodes.containsKey(task.getId())) {
+            remove(task.getId());
+            linkLast(task);
+        }
+        else {
+            linkLast(task);
+        }
+    }
+
+    @Override
+    public void remove(int id) {
+        if (taskHistoryNodes.containsKey(id)) {
+            getTasks().remove(id);
+            removeNode(taskHistoryNodes.get(id));
+            taskHistoryNodes.remove(id);
         }
     }
 
     @Override
     public List<Task> getHistory() {
-        return taskHistory;
+
+        return getTasks();
     }
 }
