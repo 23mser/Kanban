8ee82371f3ff85102fb5cf3fded36938d0bc3f48
Sergey <moskovets2319@yandex.ru>
2023-07-16 23:16:33 +0300

    добавить проверку на пустые списки

--- src/test/InMemoryTaskManagerTest.java
+++ src/test/InMemoryTaskManagerTest.java
@@ -1,7 +1,9 @@
 package test;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import service.InMemoryTaskManager;
 import utils.ManagerSaveException;
 
@@ -15,5 +17,12 @@ class InMemoryTaskManagerTest extends TaskManagerTest<InMemoryTaskManager> {
     void afterEach() throws ManagerSaveException {
         taskManager.deleteAll();
         taskManager.getHistory().clear();
+    }
+
+    @Test
+    public void allListsOfTasksIsEmpty() {
+        Assertions.assertEquals(0, taskManager.getAllTasks().size());
+        Assertions.assertEquals(0, taskManager.getAllEpics().size());
+        Assertions.assertEquals(0, taskManager.getAllSubtasks().size());
     }
 }
