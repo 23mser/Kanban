f0cb661aa823bf5001aad9137147864cdda71003
Sergey <moskovets2319@yandex.ru>
2023-07-16 23:18:17 +0300

    создать эпик для каждого теста

--- src/test/EpicTest.java
+++ src/test/EpicTest.java
@@ -3,21 +3,28 @@ package test;
 import model.Epic;
 import model.Subtask;
 import model.TaskStatus;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import service.InMemoryTaskManager;
 import utils.ManagerSaveException;
 
 public class EpicTest {
 
-    InMemoryTaskManager inMemoryTaskManager = new InMemoryTaskManager();
-    Epic epic;
-    {
-        try {
-            epic = inMemoryTaskManager.createNewEpic(new Epic("name", "description"));
-        } catch (ManagerSaveException e) {
-            throw new RuntimeException(e);
-        }
+    private static InMemoryTaskManager taskManager;
+    private static Epic epic;
+
+    @BeforeEach
+    void beforeEach() throws ManagerSaveException {
+        taskManager = new InMemoryTaskManager();
+        epic = taskManager.createNewEpic(new Epic("en", "ed"));
+    }
+
+    @AfterEach
+    void afterEach() throws ManagerSaveException {
+        taskManager.deleteAll();
+        taskManager.getHistory().clear();
     }
 
     @Test
@@ -28,47 +35,47 @@ public class EpicTest {
 
     @Test
     public void allSubtasksStatusIsNew() throws ManagerSaveException {
-        inMemoryTaskManager.createNewSubtask(epic,
+        taskManager.createNewSubtask(epic,
                 new Subtask("n1", "d1", "02.01.2000 12:00", 10, 5));
-        inMemoryTaskManager.createNewSubtask(epic,
+        taskManager.createNewSubtask(epic,
                 new Subtask("n2", "d2", "03.01.2000 12:00", 10, 5));
-        inMemoryTaskManager.updateEpicStatus(epic);
+        taskManager.updateEpicStatus(epic);
         Assertions.assertEquals(TaskStatus.NEW, epic.getStatus());
     }
 
     @Test
     public void allSubtasksStatusIsDone() throws ManagerSaveException {
         epic.setStatus(TaskStatus.NEW);
-        Subtask subtask1 = inMemoryTaskManager.createNewSubtask(epic,
+        Subtask subtask1 = taskManager.createNewSubtask(epic,
                 new Subtask("n1", "d1", "02.01.2000 12:00", 10, 5));
-        Subtask subtask2 = inMemoryTaskManager.createNewSubtask(epic,
+        Subtask subtask2 = taskManager.createNewSubtask(epic,
                 new Subtask("n2", "d2", "03.01.2000 12:00", 10, 10));
         subtask1.setStatus(TaskStatus.DONE);
         subtask2.setStatus(TaskStatus.DONE);
-        inMemoryTaskManager.updateEpicStatus(epic);
+        taskManager.updateEpicStatus(epic);
         Assertions.assertEquals(TaskStatus.DONE, epic.getStatus());
     }
 
     @Test
     public void subtasksStatusIsNewAndDone() throws ManagerSaveException {
-        inMemoryTaskManager.createNewSubtask(epic,
+        taskManager.createNewSubtask(epic,
                 new Subtask("n1", "d1", "02.01.2000 12:00", 10, 5));
-        Subtask subtask2 = inMemoryTaskManager.createNewSubtask(epic,
+        Subtask subtask2 = taskManager.createNewSubtask(epic,
                 new Subtask("n2", "d2", "03.01.2000 12:00", 10, 10));
         subtask2.setStatus(TaskStatus.DONE);
-        inMemoryTaskManager.updateEpicStatus(epic);
+        taskManager.updateEpicStatus(epic);
         Assertions.assertEquals(TaskStatus.IN_PROGRESS, epic.getStatus());
     }
 
     @Test
     public void allSubtasksStatusIsInProgress() throws ManagerSaveException {
-        Subtask subtask1 = inMemoryTaskManager.createNewSubtask(epic,
+        Subtask subtask1 = taskManager.createNewSubtask(epic,
                 new Subtask("n1", "d1", "02.01.2000 12:00", 10, 5));
-        Subtask subtask2 = inMemoryTaskManager.createNewSubtask(epic,
+        Subtask subtask2 = taskManager.createNewSubtask(epic,
                 new Subtask("n2", "d2","03.01.2000 12:00", 10, 10));
         subtask1.setStatus(TaskStatus.IN_PROGRESS);
         subtask2.setStatus(TaskStatus.IN_PROGRESS);
-        inMemoryTaskManager.updateEpicStatus(epic);
+        taskManager.updateEpicStatus(epic);
         Assertions.assertEquals(TaskStatus.IN_PROGRESS, epic.getStatus());
     }
-}
+} 
